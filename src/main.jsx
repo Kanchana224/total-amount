2f982b346ee031b8f07d83394878d9a04848672a
Kanchana224 <kkanchana427@gmail.com>
2023-12-31 12:25:46 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,8 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <React.StrictMode>
+  <App/>
+ </React.StrictMode> 
+)
